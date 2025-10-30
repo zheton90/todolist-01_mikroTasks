32b3aa4b42ae05688ede2f17521cdd1187d08352
zheton90 <jen.nikolenko@yandex.ru>
2025-10-30 19:47:10 +0300

    add 333

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -4,6 +4,7 @@ export const Post = () => {
         <div>
             <h4>Post111</h4>
             <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            <p>333</p>
         </div>
     );
 };
